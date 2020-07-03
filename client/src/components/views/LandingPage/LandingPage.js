6d19b69f033c545ec132da7c2f7b9ceac402ebdf
박지현 <pjh_5890@naver.com>
2020-07-04 00:30:57 +0900

    gridCards 부분 오류 고쳐야함

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
 import {API_URL, API_KEY, IMAGE_BASE_URL} from '../../Config';
 import MainImage from './Sections/MainImage';
+import GridCards from '../commons/GridCards';
+import {Row} from 'antd';
 
 function LandingPage() {
 
@@ -36,6 +38,18 @@ function LandingPage() {
                 <h2> Movies by latest</h2>
                 <hr/>
                 {/* Movie Grid Cards*/}
+                <Row>
+                {Movies && Movies.map((movie, index) => (
+                    <React.Fragment key={index}>
+                        <GridCards
+                            image={movie.poster_path ? `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                            movieId={movie.id}
+                            movieName={movie.original_title}
+                        />
+                    </React.Fragment>
+                ))}
+         
+                </Row>
             </div>
 
             <div style={{display: 'flex', justifyContent: 'center'}}>
---
+++ client/src/components/views/commons/GridCards.js
@@ -0,0 +1,24 @@
+import React, {useEffect} from 'react'
+import {Col} from 'antd';
+
+function GridCards(props) {
+    
+    useEffect(() => {
+        console.log(props)
+    }, [])
+
+    return (
+        <div>
+            <Col lg={6} md={8} xs={24} >
+                <div style={{position: 'relative'}}>
+                    <a href={`/movie/${props.movieId}`}>
+                     <img src={props.image} alt={props.movieName}/>
+                    </a>
+                </div>
+            </Col>
+        </div>
+    )
+}
+
+export default GridCards
+
