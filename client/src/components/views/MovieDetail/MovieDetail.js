d30314fab2682a6082f0761446ab73679ec07ea8
박지현 <pjh_5890@naver.com>
2020-07-14 20:54:18 +0900

    영화 정보들 가져오기

--- client/src/components/views/MovieDetail/MovieDetail.js
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {API_URL, API_KEY, IMAGE_BASE_URL} from '../../Config';
+import MainImage from '../LandingPage/Sections/MainImage'
+import MovieInfo from './Sections/MovieInfo'
 
-function MovieDetail() {
+function MovieDetail(props) {
+
+    let movieId = props.match.params.movieId
+    const [Movie , setMovie ] = useState([])
+    let endpointCrew = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`
+    let endpointInfo =  `${API_URL}movie/${movieId}?api_key=${API_KEY}`
+
+    useEffect(() => {
+        fetch(endpointInfo)
+            .then(response => response.json())
+            .then(response => {
+                setMovie(response)
+            })
+    }, [])
+
     return (
         <div>
-            
+            {/* Header */}
+            {Movie.backdrop_path &&
+                <MainImage
+                    image={`${IMAGE_BASE_URL}w1280${Movie.backdrop_path}`}
+                    title={Movie.original_title}
+                    text={Movie.overview}
+                />
+            }       
+
+            {/* Body */}
+            <div style={{width: '85%', margin : '1rem auto'}}>
+                {/* Movie Info */}
+                <MovieInfo movie={Movie}/>
+                <br />
+                {/* Actors Grid */}
+
+                <div style={{display : 'flex', justifyContent : 'center', margin: '2rem'}}>
+                    <button> Toggle Actor View</button>
+                </div>
+
+                
+            </div>
         </div>
     )
 }
