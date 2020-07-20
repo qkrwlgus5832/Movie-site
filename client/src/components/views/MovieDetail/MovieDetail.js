876d1a250cb8f75cd5871c1d02143250681a7992
박지현 <pjh_5890@naver.com>
2020-07-20 20:49:40 +0900

    영화 출연진 가져오기

--- client/src/components/views/MovieDetail/MovieDetail.js
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -2,11 +2,17 @@ import React, { useEffect, useState } from 'react'
 import {API_URL, API_KEY, IMAGE_BASE_URL} from '../../Config';
 import MainImage from '../LandingPage/Sections/MainImage'
 import MovieInfo from './Sections/MovieInfo'
+import GridCards from '../commons/GridCards'
+import {Row} from 'antd'
+
 
 function MovieDetail(props) {
 
     let movieId = props.match.params.movieId
     const [Movie , setMovie ] = useState([])
+    const [Casts, setCasts] = useState([]) // initial State가 array이다.
+    const [ActorToggle, setActorToggle] = useState(false)
+
     let endpointCrew = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`
     let endpointInfo =  `${API_URL}movie/${movieId}?api_key=${API_KEY}`
 
@@ -16,8 +22,18 @@ function MovieDetail(props) {
             .then(response => {
                 setMovie(response)
             })
-    }, [])
 
+        fetch(endpointCrew)
+            .then(response => response.json())
+            .then(response => {
+                console.log(response.cast)
+                setCasts(response.cast)
+            })
+
+    }, [])
+    const toggleActorView = () => {
+        setActorToggle(!ActorToggle)
+    }
     return (
         <div>
             {/* Header */}
@@ -37,10 +53,21 @@ function MovieDetail(props) {
                 {/* Actors Grid */}
 
                 <div style={{display : 'flex', justifyContent : 'center', margin: '2rem'}}>
-                    <button> Toggle Actor View</button>
+                    <button onClick={toggleActorView}> Toggle Actor View</button>
                 </div>
+                {ActorToggle &&
+                    <Row gutter={[16, 16]}>
+                        {Casts && Casts.map((cast, index) => (
+                            <React.Fragment key={index}>
+                                <GridCards
+                                    image={cast.profile_path ? `${IMAGE_BASE_URL}w500${cast.profile_path}` : null}
+                                    characterName={cast.name}
+                                />
+                            </React.Fragment>
+                        ))}
 
-                
+                    </Row>
+                }       
             </div>
         </div>
     )
--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -1,23 +1,32 @@
-import React, {useEffect} from 'react'
-import {Col} from 'antd';
+import React, { useEffect } from 'react'
+import { Col } from 'antd';
 
 function GridCards(props) {
-    
-    useEffect(() => {
-        console.log(props)
-    }, [])
-
-    return (
-        <div>
-            <Col lg={6} md={8} xs={24} >
-                <div style={{position: 'relative'}}>
-                    <a href={`/movie/${props.movieId}`}>
-                     <img style={{width: '100%', height: '320px'}} src={props.image} alt={props.movieName}/>
-                    </a>
-                </div>
-            </Col>
-        </div>
-    )
+    if (props.landingPage) {
+        return (
+            <div>
+                <Col lg={6} md={8} xs={24} >
+                    <div style={{ position: 'relative' }}>
+                        <a href={`/movie/${props.movieId}`}>
+                            <img style={{ width: '100%', height: '320px' }} src={props.image} alt={props.movieName} />
+                        </a>
+                    </div>
+                </Col>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <Col lg={6} md={8} xs={24} >
+                    <div style={{ position: 'relative' }}>
+                   
+                        <img style={{ width: '100%', height: '320px' }} src={props.image} alt={props.characterName} />
+                       
+                    </div>
+                </Col>
+            </div>
+        )
+    }
 }
 
 export default GridCards
